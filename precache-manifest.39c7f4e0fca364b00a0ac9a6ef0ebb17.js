03871499b340f7252dcd2b13bf6ee988caf42131
Anna5756855 <dolgopolovaanna5@gmail.com>
2023-02-13 18:57:06 +0300

    Deploy React app to GitHub Pages

---
+++ precache-manifest.39c7f4e0fca364b00a0ac9a6ef0ebb17.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "58ee804027a72f35e3ed",
+    "url": "/realco-app/static/css/main.2e1fcb48.chunk.css"
+  },
+  {
+    "revision": "58ee804027a72f35e3ed",
+    "url": "/realco-app/static/js/main.58ee8040.chunk.js"
+  },
+  {
+    "revision": "19227b26d4e7f150d30a",
+    "url": "/realco-app/static/js/1.19227b26.chunk.js"
+  },
+  {
+    "revision": "29739a60909893472085",
+    "url": "/realco-app/static/js/2.29739a60.chunk.js"
+  },
+  {
+    "revision": "d38f3268b81d2c49a4a3",
+    "url": "/realco-app/static/js/runtime~main.d38f3268.js"
+  },
+  {
+    "revision": "6696c0487356f0b66e738acaa9d7715c",
+    "url": "/realco-app/static/media/logo.6696c048.svg"
+  },
+  {
+    "revision": "276e43b135ca280c1e3addb8f5acd122",
+    "url": "/realco-app/static/media/template.276e43b1.jpeg"
+  },
+  {
+    "revision": "8c290ab44d3c617e0100492062e6ffed",
+    "url": "/realco-app/static/media/preloader.8c290ab4.svg"
+  },
+  {
+    "revision": "a0eaf404f12a5a68bc23cb29e431914e",
+    "url": "/realco-app/index.html"
+  }
+];
